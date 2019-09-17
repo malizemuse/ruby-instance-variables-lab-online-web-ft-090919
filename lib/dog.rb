670c0695b0f3a6c9f04d66ac1f4f1a15e873a9ac
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-17 20:45:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
   
   # Setter
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
  
   # Getter
   def name
-    this_dogs_name
+    @this_dogs_name
   end
   
 end
