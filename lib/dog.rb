bdcdebfa88f1332937483e7e26f026effdbdffd4
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-17 20:46:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  
+  # Setter
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  # Getter
+  def name
+    @this_dogs_name
+  end
+  
+end  
+
+
+#
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+  
